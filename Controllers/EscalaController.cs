1a2491807c0999addcb7afc88944ce889f9bcf1d
eduardovscruuz <eduardovscruuz@gmail.com>
2025-08-26 15:14:08 -0300

    Controller de escalas: OK

--- Controllers/EscalaController.cs
+++ Controllers/EscalaController.cs
@@ -1,10 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ComexApi.Models;
+using ComexApi.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ComexApi.Controllers;
 
 [ApiController]
-[Route("api/[controller]")]
+[Route("escalas")]
 public class EscalaController : ControllerBase
 {
+    private readonly IEscalaService _service;
+    public EscalaController(IEscalaService service) => _service = service;
 
+
+    [HttpGet]
+    public async Task<IActionResult> Listar() => Ok(await _service.ListarEscalas());
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> Buscar(int id)
+    {
+        var escala = await _service.BuscarEscalaPorId(id);
+        return Ok(escala);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Criar([FromBody] Escala escala)
+    {
+        var criada = await _service.CriarEscala(escala);
+        return CreatedAtAction(nameof(Buscar), new { id = criada.Id }, criada);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Deletar(int id)
+    {
+        if (!await _service.DeletarEscala(id)) return NotFound();
+        return NoContent();
+    }
 }
