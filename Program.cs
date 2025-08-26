809f04500d1c4bed752ba28a5a591251b744ff6c
eduardovscruuz <eduardovscruuz@gmail.com>
2025-08-26 18:18:58 -0300

    Cors configurado

--- Program.cs
+++ Program.cs
@@ -15,8 +15,21 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
+
 
 var app = builder.Build();
+
+app.UseCors("AllowAll");
 
 if (app.Environment.IsDevelopment())
 {
