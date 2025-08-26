3ba10878430ace4219664ac6df9247a5261eedf0
eduardovscruuz <eduardovscruuz@gmail.com>
2025-08-26 12:52:04 -0300

    Service de manifestos ok

--- Services/ManifestoService.cs
+++ Services/ManifestoService.cs
@@ -1,12 +1,15 @@
 ﻿using ComexApi.Data;
 using ComexApi.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace ComexApi.Services;
 
 public interface IManifestoService
 {
-    Task<List<Manifesto>> GetAllManifestos();
-    Task<Manifesto> GetManifestoById(int id);
+    Task<List<Manifesto>> ListarManifestos();
+    Task<Manifesto?> BuscarManifestoPorId(int id);
+    Task<Manifesto> CriarManifesto(Manifesto manifesto);
+    Task<bool> DeletarManifesto(int id);
 }
 
 public class ManifestoService : IManifestoService
@@ -18,13 +21,25 @@ public class ManifestoService : IManifestoService
         _context = context;
     }
 
-    public Task<List<Manifesto>> GetAllManifestos()
+    public async Task<List<Manifesto>> ListarManifestos() => await _context.TabelaDeManifestos.ToListAsync();
+
+    public async Task<Manifesto?> BuscarManifestoPorId(int id) => await _context.TabelaDeManifestos.FindAsync(id);
+
+    public async Task<Manifesto> CriarManifesto(Manifesto manifesto)
     {
-        throw new NotImplementedException();
+        _context.TabelaDeManifestos.Add(manifesto);
+        await _context.SaveChangesAsync();
+        return manifesto;
     }
 
-    public Task<Manifesto> GetManifestoById(int id)
+    public async Task<bool> DeletarManifesto(int id)
     {
-        throw new NotImplementedException();
+        var existente = await _context.TabelaDeManifestos.FindAsync(id);
+        if (existente == null) return false;
+
+        _context.TabelaDeManifestos.Remove(existente);
+        await _context.SaveChangesAsync();
+        return true;
     }
+
 }
